e43b65ca10e8ddcd2e5e44f46a3c872fd8b70b13
efffi <efinoam96@gmail.com>
2018-06-10 21:45:48 +0300

    add new function to make-it-pass file

--- test/make-it-pass.test.js
+++ test/make-it-pass.test.js
@@ -9,3 +9,8 @@ test('sum should return the addition of two numbers', function (t) {
   t.equal(3, sum(1, 2)); // make this test pass by completing the add function!
   t.end();
 });
+
+function mult (c, d) {
+	return d*c;
+  // your code to make the test pass goes here ...
+}
